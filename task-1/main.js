714052d148a73600d31ecfd087ab3bf4a88037c5
agacan123 <110633844+agacan123@users.noreply.github.com>
2022-08-30 10:23:59 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -4,6 +4,21 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+  let numbers = result.input.split(" ")
+  let count = numbers. length
+
+  for (let i = 0; i <numbers.length; i++){
+   const element= parseInt(numbers[i])
+
+for (let j =i +1; j< numbers.length; j++){
+
+const next_element =parseInt(numbers[j])
+if (element === next_element){
+
+    count--
+}
+}
+
+console.log(count);
   
 });
