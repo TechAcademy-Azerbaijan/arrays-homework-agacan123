cdadfc5c486a74e3aa4cbb2e9c3f06978c02a563
agacan123 <110633844+agacan123@users.noreply.github.com>
2022-08-31 23:23:40 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -4,6 +4,19 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    
+    let numbers = result.input.split(" ")
+    let arr =[]
+    for (let i= 0; i<numbers.length -1; i++){
+const element = parseInt(element[i])
+const next_element = parseInt( element[i+1])
+if ( element < next_element){
+
+    arr.push(next_element)
+}
+
+
+    }
+    console.log(numbers [i+1]);
   
 });
