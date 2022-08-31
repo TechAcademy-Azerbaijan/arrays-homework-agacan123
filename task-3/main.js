9f607a8763313868ab6b8826bc1bcbb149ca2533
agacan123 <110633844+agacan123@users.noreply.github.com>
2022-08-31 23:21:32 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,16 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+let n = result.input.split(" ")
+let arr =[]
+for (let i=0; i<n.length; i++){
+
+if (i % 2 == 0){
+arr.push (n[i])
+
+}
+
+}
+console.log(arr.join(' '));
   
 });
